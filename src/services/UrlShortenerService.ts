a2e3fdf8a99040f39d044fad198f574206fe0613
AKEJU FATAI <fatai@nestcoin.com>
2022-03-21 11:55:06 +0100

    fix issues

---
+++ src/services/UrlShortenerService.ts
@@ -0,0 +1,14 @@
+import { BadRequestError } from "../exceptions";
+
+/**
+ * @class UrlShortenerService
+ */
+class UrlShortenerService {
+
+    static async createShortUrl(longUrl: string): Promise<void> {
+        
+    }
+
+}
+
+export default UrlShortenerService;
